c3023541f2d5a7be9f5f1417a5ff011d7dc6302c
Xairon <ringuetnicolas2@gmail.com>
2019-03-23 17:27:10 +0100

    query

--- src/queryRDF.java
+++ src/queryRDF.java
@@ -1,5 +1,4 @@
-import org.apache.jena.query.QueryExecutionFactory;
-import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
@@ -16,22 +15,19 @@ public class queryRDF{
         InputStream in = FileManager.get().open("human.rdf");
         if (in != null)
             model.read(in, "");
-        String rdf = "http://www.inria.fr/2007/09/11/humans.rdfs#";
-        Property myitemvalue1 = model.createProperty( rdf + "Person" );
-        Property myitemvalue2 = model.createProperty( rdf + "Man" );
+
 
         // Run the SPARQL query and get some results
         String getItemsLists = "" +
-                "prefix rdf: <http://www.inria.fr/2007/09/11/humans.rdfs>\n" +
+                "prefix rdf: <http://www.inria.fr/2007/09/11/humans.rdfs#>\n" +
                 "select ?x ?t where\n"+
                 "{\n"+
                 "?x rdf:type ?t\n"+
                 "}";
-        ResultSet results = QueryExecutionFactory.create( getItemsLists, model ).execSelect();
 
-        // For each solution in the result set
-        while ( results.hasNext() ) {
-
-        }
+        Query query = QueryFactory.create(getItemsLists);
+        QueryExecution qExe = QueryExecutionFactory.create(query, model);
+        ResultSet result = qExe.execSelect();
+        ResultSetFormatter.out(System.out, result, query) ;
     }
 }
