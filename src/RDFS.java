108757d93d8e44132f0dae9d984761c124a2ba22
Louis Rousselot de Saint Ceran <louis.desaintceran@gmail.com>
2019-03-28 19:31:28 +0100

    preuve inference query avec les deux models (rdsf et sans)

--- src/RDFS.java
+++ src/RDFS.java
@@ -1,3 +1,4 @@
+import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.reasoner.Reasoner;
 import org.apache.jena.reasoner.ReasonerRegistry;
@@ -28,6 +29,24 @@ public class RDFS {
                         " has property " + stmt.getPredicate().getLocalName() +
                         " with value " + stmt.getObject());
             }
+
+            System.out.println("////////////////////////////////////");
+
+            String test = "prefix rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"+
+                    "prefix prop:<http://www.inria.fr/2007/09/11/humans.rdfs#>\n"+
+                    "select ?y  where {\n"+
+                    "?y rdf:type prop:Male\n"+
+                    "}";
+
+            Query query = QueryFactory.create(test);
+            QueryExecution qExe = QueryExecutionFactory.create(query, model);
+            ResultSet result = qExe.execSelect();
+            ResultSetFormatter.out(System.out, result, query) ;
+
+            Query query2 = QueryFactory.create(test);
+            QueryExecution qExe2 = QueryExecutionFactory.create(query2, infmodel);
+            ResultSet result2 = qExe2.execSelect();
+            ResultSetFormatter.out(System.out, result2, query2) ;
 
         }
     }
