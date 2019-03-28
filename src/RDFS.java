9a103cbf0885215e4b6a52dcaeac830502c90fca
xairon <ringuetnicolas2@gmail.com>
2019-03-28 14:19:02 +0100

    RDFS model

---
+++ src/RDFS.java
@@ -0,0 +1,33 @@
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.reasoner.Reasoner;
+import org.apache.jena.reasoner.ReasonerRegistry;
+import org.apache.jena.util.FileManager;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class RDFS {
+
+        public static void main(String[] args) throws IOException {
+            // Create a model and load the data
+            Model model = ModelFactory.createDefaultModel();
+            Model schema = ModelFactory.createOntologyModel();
+            InputStream in = FileManager.get().open("human.rdf");
+            InputStream in2 = FileManager.get().open("human.rdfs");
+
+            if (in != null)
+                model.read(in, "");
+            if (in != null)
+                schema.read(in2, "");
+            Reasoner reasoner = ReasonerRegistry.getRDFSReasoner();
+            reasoner = reasoner.bindSchema(schema);
+            InfModel infmodel = ModelFactory.createInfModel(reasoner, model);
+            for (StmtIterator i = infmodel.listStatements(); i.hasNext(); ) {
+                Statement stmt = i.next();
+                System.out.println("Resource " + stmt.getSubject().getLocalName()/*getURI()*/ +
+                        " has property " + stmt.getPredicate().getLocalName() +
+                        " with value " + stmt.getObject());
+            }
+
+        }
+    }
